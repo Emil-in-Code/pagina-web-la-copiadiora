167844db221bac10862147bb8790499c7e48f222
JoanB-dev <joanbernabe94@gmail.com>
2025-03-17 16:08:06 -0300

    17/03/2025
    Se avanzó con la validación

--- js la copiadora/registro.js
+++ js la copiadora/registro.js
@@ -3,7 +3,7 @@ const formulario = document.getElementById("formulario");
 const inputs = document.querySelectorAll("#formulario input")
 
 const expresiones = {
-	username:  /^[a-zA-Z0-9À-ÿ]{1,40}$/, // Letras, números, sin espacios
+    username: /^[a-zA-ZÀ-ÿ]+(?:\s[a-zA-ZÀ-ÿ]+)*$/,
 	lastname:  /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras, espacios y acentos
     email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	contraseña: /^.{4,12}$/, // 4 a 12 digitos.
@@ -14,19 +14,19 @@ const expresiones = {
 const validarFormulario = (e) => {
     switch (e.target.name) {
         case "username":
-            
+            if(expresiones.username.test())
         break;
 
         case "lastname":
-            
+            if(expresiones.lastname.test())
         break;
 
         case "email":
-            
+            if(expresiones.email.test())
         break;
 
         case "whatsApp":
-            
+            if(expresiones.whatsApp.test())
         break;
 
         case "birthdate":
