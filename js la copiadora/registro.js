06485d085df69647332d971b52e3214fab87e02d
JoanB-dev <joanbernabe94@gmail.com>
2025-04-10 15:36:33 -0300

    10/04/2025
    se empezó la validación del lado del cliente

--- js la copiadora/registro.js
+++ js la copiadora/registro.js
@@ -1,9 +1,20 @@
+const inputNombre = document.getElementById("nombre");
+const inputApellido = document.getElementById("apellido");
+const inputMail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
+console.log(inputNombre,inputApellido,inputMail,inputPassword);
 
-const formulario = document.getElementById("formulario");
-const inputs = document.querySelectorAll("#formulario input")
+inputNombre.addEventListener("blur", function(e){
+    const nombreValue = e.target.value;
+    if (nombreValue.length === 0) {
+        console.log("pone el nombre")
+    }
+      
+})
+
 
 const expresiones = {
-    username: /^[a-zA-ZÀ-ÿ]+(?:\s[a-zA-ZÀ-ÿ]+)*$/,
+    nombre: /^[a-zA-ZÀ-ÿ]+(?:\s[a-zA-ZÀ-ÿ]+)*$/,
 	lastname:  /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras, espacios y acentos
     email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	contraseña: /^.{4,12}$/, // 4 a 12 digitos.
@@ -11,44 +22,3 @@ const expresiones = {
     birthdate: /^\d{4}-\d{2}-\d{2}$/,
 }
 
-const validarFormulario = (e) => {
-    switch (e.target.name) {
-        case "username":
-            if(expresiones.username.test(e.target.value)){
-                document.getElementById("grupo__username").classList.remove("formulario__grupo-incorrecto");
-                document.getElementById("grupo__username").classList.add("formulario__grupo-correcto");
-                
-            }else {
-                document.getElementById("grupo__username").classList.add("formulario__grupo-incorrecto");
-                document.getElementById("grupo__username").classList.remove("formulario__grupo-correcto");
-            }
-        break;
-
-        case "lastname":
-            if(expresiones.lastname.test())
-        break;
-
-        case "email":
-            if(expresiones.email.test())
-        break;
-
-        case "whatsApp":
-            if(expresiones.whatsApp.test())
-        break;
-
-        case "birthdate":
-            
-        break;
-
-    }
-}
-
-inputs.forEach((input) => {
-    input.addEventListener("keyup", validarFormulario);
-    input.addEventListener("blur", validarFormulario);
-});
-
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-})
