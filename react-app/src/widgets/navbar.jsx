a8378e4eac24f216230bf82a66fd5ed80c2ad177
Emilio Flor <joanbernabe94@gmail.com>
2025-08-18 14:49:43 -0300

    WIP: acomodando rutas del nav

--- react-app/src/widgets/navbar.jsx
+++ react-app/src/widgets/navbar.jsx
@@ -1,31 +1,32 @@
 // src/widgets/navbar.jsx
 
 import React from 'react'
+import {link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <header className="header__container">
       <div className="logo__container--nav">
-        <a className="nav__logo" href="index.html">La Copiadora</a>
+        <a className="nav__logo" href="Index.jsx">La Copiadora</a>
       </div>
       
       <input type="checkbox" className="header__checkbox" id="open-menu" />
       <label htmlFor="open-menu" className="header__open-nav-button" role="button">☰</label>
 
-      <nav className="nav" aria-label="Main Navigation">
+      <nav className="nav" aria-label="Main Navigation"> 
         <div className="header__nav">
           <ul className="header__nav--list">
             <li><a className="nav__item" href="laCopiadoraStore.html" target="_blank">Store</a></li>
-            <li><a className="nav__item" href="envios.html">Envíos</a></li>
-            <li><a className="nav__item" href="pricing.html">Precios</a></li>
-            <li><a className="nav__item" href="/react/pedidos">Pedidos</a></li>
-            <li><a className="nav__item" href="linksutiles.html">Links Útiles</a></li>
-            <li><a className="nav__item" href="Register.html">Registrarme</a></li>
-            <li><a className="nav__item nav__item--button" href="login.html">Log-in</a></li>
+            <li><Link className="nav__item" to="/envios">Envíos</Link></li>
+            <li><Link className="nav__item" to="/precios">Precios</Link></li>
+            <li><Link className="nav__item" to="/pedidos">Pedidos</Link></li>
+            <li><Link className="nav__item" to="links-utiles">Links Útiles</Link></li>
+            <li><Link className="nav__item" href="Register.html">Registrarme</Link></li>
+            <li><Link className="nav__item nav__item--button" href="login.html">Log-in</Link></li>
             <li id="usuario-navbar" className="nav__item usuario-navbar oculto" style={{position: 'relative'}}>
               <span id="nombre-usuario">👤</span>
               <ul id="dropdown-menu" className="dropdown-menu oculto">
-                <li><a href="#" id="logout-link">Cerrar sesión</a></li>
+                <li><Link href="#" id="logout-link">Cerrar sesión</Link></li>
               </ul>
             </li>
           </ul>
