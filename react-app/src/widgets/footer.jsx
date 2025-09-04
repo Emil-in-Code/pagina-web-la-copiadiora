508f8e23a51748ccfdd17cb68f20a163e502b9db
Emilio Flor <joanbernabe94@gmail.com>
2025-09-04 14:17:54 -0300

    WIP:footer
    se empezó el footer

--- react-app/src/widgets/footer.jsx
+++ react-app/src/widgets/footer.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <nav>
-      <div>footer en desarrollo...</div>
-    </nav>
+    <div className = "footer-container">
+      <div className ="logo-container">
+         <Link className="nav__logo" to="/">La Copiadora</Link>
+      </div>
+      <div className = "contact-container">
+        <ul>
+          <li className = "item">
+            
+          </li>
+          <li className = "item">
+            
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
